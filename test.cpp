f17f6a3537d7f7f1edf5efb375c0b3ef23777079
Peei <valenpeei@gmail.com>
2015-09-07 23:45:37 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,12 +3,26 @@
 #include "main.cpp"
 using namespace std;
 TEST_CASE() {
-	Medio a;
+	Medio m1, m2, m3;
+	Comun c;
 	colectivo c1("136","semtur",123123);
-	colectivo c2("136","semtur",132123);
-        a.Recarga(100);
-        REQUIRE(a.Saldo()==100);
-	REQUIRE(a.PagarBoleto(c1,fecha(22))==true);
+	colectivo c2("136","semtur",123456);
+        c.Recarga(10);
+        m1.Recarga(100);
+        m2.Recarga(197);
+        m3.Recarga(368);
+        
+	
+        REQUIRE(m3.Saldo()==460);
+        REQUIRE(m2.Saldo()==231);
+        REQUIRE(m1.Saldo()==100);
+	REQUIRE(m1.PagarBoleto(c1,fecha(22))==true && m1.Saldo()==97.10);
+	REQUIRE(c.PagarBoleto(c1,fecha(23))== true && c.Saldo== 4.25);
+	
+	
 	REQUIRE(c1==c2);
+	
+	
 
 }
+
